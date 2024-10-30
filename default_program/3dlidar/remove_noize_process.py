d30cd1feddc1d76cac620eb55feb82a2df35345e
KaiFujiwara55 <124759453+KaiFujiwara55@users.noreply.github.com>
2024-10-30 15:28:42 +0900

    ノイズ除去して、tmpフォルダーに保存する処理を切り出し

---
+++ default_program/3dlidar/remove_noize_process.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import pcl
+import plot
+import glob
+import time
+import matplotlib.pyplot as plt
+import default_method
+import original_method
+import get_pcd_information
+import create_gif
+
+# sec_list = ["015", "02", "025", "03"]
+sec_list = ["01"]
+for sec in sec_list:
+    dirs = glob.glob(f"/Users/kai/大学/小川研/LiDAR_step_length/20241028/pcd_{sec}s/*")
+
+    # ノイズ除去のクラスをインスタンス化
+    def_method = default_method.cloud_method()
+    ori_method = original_method.cloud_method()
+    for dir in dirs:
+        if "repeat" in dir:
+            continue
+        
+        # pcdファイルの情報を取得
+        pcd_info_list = get_pcd_information.get_pcd_information()
+        pcd_info_list.load_pcd_dir(dir)
+
+        print(f"処理開始 : {pcd_info_list.dir_name}_{sec}s")
+        start = time.time()
+
+        time_cloud = []
+        time_area_points_list = []
+        time_area_center_point_list = []
+        count = 0
+
+        # LiDAR自体の傾きを取得
+        theta_x, theta_y, theta_z = ori_method.cloud_get_tilt(pcd_info_list, upper_threshold=2000-1300)
+        for cloud, cloud_name in zip(pcd_info_list.cloud_list, pcd_info_list.cloud_name_list):
+            print(cloud_name, cloud.size)
+            # LiDAR自体の傾きを補正
+            cloud = def_method.rotate_cloud(cloud, -theta_x, theta_y)
+
+            source_cloud = cloud
+            filtered_cloud = cloud
+
+            # 高さの正規化を行う
+            source_cloud = def_method.get_cloud(def_method.get_points(source_cloud) + np.array([0, 0, 1300]))
+            filtered_cloud = def_method.get_cloud(def_method.get_points(filtered_cloud) + np.array([0, 0, 1300]))
+
+            tmp_cloud_list = []
+            for i in range(10):
+                # knnを使用
+                # indices, sqr_distances = def_method.kdtree_search(source_cloud, k=10)
+
+                # ダウンサンプリング
+                grid_filtered_cloud = def_method.voxel_grid_filter(filtered_cloud, leaf_size=(500, 500, 500))
+
+                # 半径外れ値除去
+                # filtered_cloud = def_method.radius_outlier_removal(source_cloud, radius=100, min_neighbors=2)
+
+                # 統計的外れ値除去
+                # filtered_cloud = def_method.statistical_outlier_removal(filtered_cloud, mean_k=50, std_dev_mul_thresh=1.0)
+
+                # 平面の抽出
+                ksearch = 50
+                distance_threshold = 50
+                cloud_plane, cloud_non_plane, coefficients, indices = def_method.segment_plane(grid_filtered_cloud, ksearch, distance_threshold)
+                if cloud_plane.size==0:
+                    print("No plane")
+                    filtered_cloud = tmp_cloud_list[-3]
+                    break
+                cloud_plane, cloud_non_plane = def_method.filter_points_by_distance(filtered_cloud, coefficients, distance_threshold)
+
+                
+                filtered_cloud = cloud_non_plane
+                tmp_cloud_list.append(filtered_cloud)
+            # 統計的外れ値除去
+            mean_k = 50
+            std_dev_mul_thresh = 1.0
+            statistical_filtered_cloud = def_method.statistical_outlier_removal(filtered_cloud, mean_k, std_dev_mul_thresh)
+
+            # ダウンサンプリング
+            # leaf_size = (500, 500, 100)
+            # grid_filtered_cloud = def_method.voxel_grid_filter(statistical_filtered_cloud, leaf_size)
+
+            # 高さの分散を取得
+            # var_threshold = 10000
+            # x_step = 500
+            # y_step = 500
+            # var_filtered_cloud, surface_xy_list = def_method.filter_by_height_var(grid_filtered_cloud, var_threshold, x_step, y_step)
+
+            # 領域内の点群を取得
+            area_points_list, area_center_point_list = ori_method.get_neighborhood_points(statistical_filtered_cloud, radius=250)
+
+            # 時系列の点群を保存
+            time_cloud.append(statistical_filtered_cloud)
+            time_area_points_list.append(area_points_list)
+            time_area_center_point_list.append(area_center_point_list)
+
+        print(f"処理時間 : {time.time()-start}")
+
+        # 処理結果を保存
+        area_path = f"/Users/kai/大学/小川研/LiDAR_step_length/tmp_folder/time_area_points_list/{pcd_info_list.dir_name}_{sec}s"
+        center_path = f"/Users/kai/大学/小川研/LiDAR_step_length/tmp_folder/time_area_center_point_list/{pcd_info_list.dir_name}_{sec}s"
+        ori_method.save_original_data(time_area_points_list, time_area_center_point_list, area_path, center_path)
