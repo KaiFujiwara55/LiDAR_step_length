4854232e1ded9b87f441d0af5cb378a44a78fa19
KaiFujiwara55 <124759453+KaiFujiwara55@users.noreply.github.com>
2024-11-19 10:35:49 +0900

    ピーク抽出

--- default_program/2dlidar/get_step_length.py
+++ default_program/2dlidar/get_step_length.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 from tqdm import tqdm
+from scipy.signal import find_peaks
 
 sys.path.append("/Users/kai/大学/小川研/Lidar_step_length")
 from default_program.class_method import get_pcd_information
@@ -75,8 +76,11 @@ for sec in sec_list:
                     if group_idx_2d not in left_leg_list.keys():
                         left_leg_list[group_idx_2d] = []
                         right_leg_list[group_idx_2d] = []
-                    left_leg_list[group_idx_2d].append([time_idx, np.mean(left_leg, axis=0)])
-                    right_leg_list[group_idx_2d].append([time_idx, np.mean(right_leg, axis=0)])
+                    
+                    if len(left_leg)>0:
+                        left_leg_list[group_idx_2d].append([time_idx, np.mean(left_leg, axis=0)])
+                    if len(right_leg)>0:
+                        right_leg_list[group_idx_2d].append([time_idx, np.mean(right_leg, axis=0)])
 
                     
                     normalized_left_leg = normalized_points[normalized_points[:, 0] < 0]
@@ -103,29 +107,74 @@ for sec in sec_list:
                 output_path = f"/Users/kai/大学/小川研/LiDAR_step_length/gif/2d/{pcd_info_2d.dir_name}_0025s.gif"
                 gif.create_gif(output_path, duration=0.025)
 
-        left_speed_list = []
-        right_speed_list = []
+
         for group_idx in left_leg_list.keys():
-            for i in range(len(left_leg_list[group_idx_2d])):
+            left_speed_list = []
+            for i in range(1, len(left_leg_list[group_idx])):
                 if i == 0:
                     continue
-                left_speed = (ori_method.calc_points_distance(left_leg_list[group_idx_2d][i][1], left_leg_list[group_idx_2d][i-1][1])) / (sec_2*(left_leg_list[group_idx_2d][i][0] - left_leg_list[group_idx_2d][i-1][0]))
-                right_speed = (ori_method.calc_points_distance(right_leg_list[group_idx_2d][i][1], right_leg_list[group_idx_2d][i-1][1])) / (sec_2*(right_leg_list[group_idx_2d][i][0] - right_leg_list[group_idx_2d][i-1][0]))
-
-                left_speed_list.append(left_speed)
-                right_speed_list.append(right_speed)
+                left_step = left_leg_list[group_idx][i][0] - left_leg_list[group_idx][i-1][0]
+                left_speed = (ori_method.calc_points_distance(left_leg_list[group_idx][i][1], left_leg_list[group_idx][i-1][1])) / (sec_2*left_step)
+                left_speed_list.append([left_leg_list[group_idx][i][0], left_speed])
+                # if left_step == 1:
+                #     left_speed_list.append([left_leg_list[group_idx][i][0], left_speed])
+                # else:
+                #     before_time_idx = left_speed_list[-1][0]
+                #     before_left_speed = left_speed_list[-1][1]
+                #     step_speed = (left_speed - before_left_speed) / left_step
+                #     for j in range(left_step):
+                #         left_speed_list.append([before_time_idx+(j+1), before_left_speed + step_speed*(j+1)])
             
+            right_speed_list = []
+            for i in range(1, len(right_leg_list[group_idx])):
+                if i == 0:
+                    continue
+                right_step = right_leg_list[group_idx][i][0] - right_leg_list[group_idx][i-1][0]
+                right_speed = (ori_method.calc_points_distance(right_leg_list[group_idx][i][1], right_leg_list[group_idx][i-1][1])) / (sec_2*right_step)
+                right_speed_list.append([right_leg_list[group_idx][i][0], right_speed])
+                # if right_step == 1:
+                #     right_speed_list.append([right_leg_list[group_idx][i][0], right_speed])
+                # else:
+                #     before_time_idx = right_speed_list[-1][0]
+                #     before_right_speed = right_speed_list[-1]
+                #     step_speed = (right_speed - before_right_speed) / right_step
+                #     for j in range(right_step):
+                #         right_speed_list.append([before_time_idx+(j+1), before_right_speed + step_speed*(j+1)])
+
+                # if (left_step != 1) or (right_step != 1):
+                #     print(left_step, right_step)
+
+            left_speed_list = np.array(left_speed_list)
+            right_speed_list = np.array(right_speed_list)
+
             # 移動平均を取る
             window = 10
-            left_speed_list = np.convolve(left_speed_list, np.ones(window)/window, mode='valid')
-            right_speed_list = np.convolve(right_speed_list, np.ones(window)/window, mode='valid')
+            left_speed_list[:, 1] = np.convolve(left_speed_list[:, 1], np.ones(window)/window, mode='same')
+            right_speed_list[:, 1] = np.convolve(right_speed_list[:, 1], np.ones(window)/window, mode='same')
 
-            plt.figure()
-            plt.plot(left_speed_list, label="left")
-            plt.plot(right_speed_list, label="right")
-            plt.suptitle(f"{pcd_info_2d.dir_name}")
-            plt.xlabel("time")
-            plt.ylabel("speed (mm/s)")
+            # ピークの取得
+            ######################################################################
+            left_speed_mean = np.mean(left_speed_list[:, 1])
+            right_speed_mean = np.mean(right_speed_list[:, 1])
+
+            # 平均速度で反転させて、scipyを使ってピークを取得
+            inverted_left_speed_list = left_speed_mean + (left_speed_mean - left_speed_list[:, 1])
+            inverted_right_speed_list = right_speed_mean + (right_speed_mean - right_speed_list[:, 1])
+
+            left_peaks, _ = find_peaks(inverted_left_speed_list, height=left_speed_mean, prominence=100)
+            right_peaks, _ = find_peaks(inverted_right_speed_list, height=right_speed_mean, prominence=100)
+            ######################################################################
+            
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+
+            ax.plot(left_speed_list[:, 0], left_speed_list[:, 1], label="left")
+            ax.scatter(left_speed_list[:, 0][left_peaks], left_speed_list[:, 1][left_peaks], c="green", label="left_peak", s=10)
+
+            ax.plot(right_speed_list[:, 0], right_speed_list[:, 1], label="right")
+            ax.scatter(right_speed_list[:, 0][right_peaks], right_speed_list[:, 1][right_peaks], c="red", label="right_peak", s=10)
+
             plt.legend()
+            plt.suptitle(f"{pcd_info_2d.dir_name}")
             plt.show()
             plt.close()
